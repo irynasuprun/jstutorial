e0f9f747613113b2c84a6ac1d3cbc44df1751dad
Iryna Suprun <iryna.suprun@gmail.com>
2019-04-08 22:28:25 -0400

    arrow functions

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -89,13 +89,33 @@ ages6 = years.map((el, index) => {
 });
 
 
-
 console.log(ages6);
 
+//es5
+/*
+var box5 = {
+    color: 'green',
+    position: 1,
+    clickMe: function(){
+        var self  =  this;
+        document.querySelector('.green').addEventListener('click', function(){var str = 'this is box number '+ self.position + ' and it is '+ self.color; alert(str);});
+    }
+}
 
+box5.clickMe();
+*/
 
+//es6
 
+const box6 = {
+    color: 'green',
+    position: 1,
+    clickMe: function(){
+        document.querySelector('.green').addEventListener('click', ()=>{var str = 'this is box number '+ this.position + ' and it is '+ this.color; alert(str);});
+    }
+}
 
+box6.clickMe();
 
 
 
