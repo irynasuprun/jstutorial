83c5d0921c6e76e39c68d8f5356f6a89d3ba0556
Iryna Suprun <iryna.suprun@gmail.com>
2019-04-09 22:52:03 -0400

    Arrays

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -62,6 +62,7 @@ console.log(c);
 */
 
 //Arrow functions
+/*
 const years = [1990, 1965, 1982, 1937];
 
 //ES5
@@ -90,7 +91,7 @@ ages6 = years.map((el, index) => {
 
 
 console.log(ages6);
-
+*/
 //es5
 /*
 var box5 = {
@@ -130,6 +131,7 @@ const box6 = {
 box6.clickMe();
 
 */
+/*
 
 function Person(name){
     this.name = name;
@@ -160,22 +162,91 @@ function(friends){
 
 new Person('Mike').myFriends6(friends);
 
+*/
+
+/***************
+//Destructuring
+//ES5
+var johm = ['John', 28];
+//var name = johm[0];
+//var age = john[1];
+
+//ES6
+const [name, year] = ['John', 26]
+console.log(name);
+console.log(year);
+
+const obj = {
+    firstName: 'john',
+    lastname: 'Smith'
+}
+
+const {firstName, lastname} = obj;
+console.log(firstName);
+console.log(lastname);
 
 
+const {firstName:a, lastname: b} = obj;
+
+console.log(a);
+console.log(b);
 
 
+function caclAgeRetirement(year){
+    const age = new Date().getFullYear - year;
+    return [age, 65-age];
+}
+
+const [age2, retirement] = caclAgeRetirement(1990);
+
+console.log(age2);
+console.log(retirement);
+
+******************************/
+
+//Array
+const boxes = document.querySelectorAll('.box');
+//ES5
+/* 
+var boxesArr5 = Array.prototype.slice.call(boxes);
+boxesArr5.forEach(function(cur){
+    cur.style.backgroundColor = 'dodgerblue';
+});
+*/
+
+//ES6
+const boxesArr6 = Array.from(boxes);
+boxesArr6.forEach(cur => cur.style.backgroundColor = 'dodgerblue')
+
+//ES5
+/*
+for (var i = 0; i < boxesArr5.length; i ++)
+{
+        if(boxesArr5[i].className === 'box blue'){
+            continue;
+        }
+        boxesArr5[i].textContent = 'I changed to blue!';
+}
+*/
+//Es6
+for (const cur of boxesArr6){
+    if (cur.className.includes('blue')){
+        continue;
+    }
+    cur.textContent = 'I changed to blue!';
+}
+
+//ES5 
+var ages = [7, 12, 25, 11, 17];
+var full = ages.map(function(cur){
+    return cur >= 18;
+})
+
+console.log(full);
+console.log(full.indexOf(true));
+console.log(ages[full.indexOf(true)]);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+//ES6
+console.log(ages.findIndex(cur => cur>=18))
+console.log(ages.find(cur => cur>=18))
